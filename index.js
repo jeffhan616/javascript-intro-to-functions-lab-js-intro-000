6651730c5a1a22ad87bd250442678a5dfb72a804
Jeff Han <jeff1fbc@gmail.com>
2018-07-20 22:36:48 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string){
+  return string.toLowerCase();
+}
+
+function logShout(string)
+{   
+  return console.log(shout(string)) ;
+
+  
+  
+}
+function sayHiToGrandma(string)
+ {
+   var Str2="";
+ Str2=string.toLowerCase(string);
+if (Str2===string)
+{
+
+return "I can't hear you!" ;
+}
+}
